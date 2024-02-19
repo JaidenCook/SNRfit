4f612e845afa99b7ce36486e2e6e94ef64eccc76
JaidenCook <jaiden.cook@student.curtin.edu.au>
2024-02-19 14:47:28 +0800

    Added analytic Gauss conversion funcs

--- gaus_decv.py
+++ gaus_decv.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def deconv2(gaus_bm, gaus_c):
     """ 
     Deconvolves gaus_bm from gaus_c to give gaus_dc.
@@ -59,3 +61,132 @@ def deconv2(gaus_bm, gaus_c):
             bpa = rad * 0.5 * atan2(-gamma, alpha-beta)
         ifail = 0
     return (bmaj, bmin, bpa), ifail
+
+def gausslm_sig2abc(sigl,sigm,pa):
+    """
+    Calculate the 2D Gaussian parameters in the polynomial ax**2 + 2bxy +cy**2.
+
+    Parameters:
+    ----------
+    sigl : float
+        Gaussian major axis.
+    sigm : float
+        Gaussian minor axis.
+    pa : float
+        Gaussian position angle in radians. 
+
+    Returns:
+    ----------
+    a : float
+        x axis coefficient.
+    b : float
+        correlation coefficient.
+    c : float
+        y axis coefficient.
+    """
+
+    a = (np.cos(pa)**2)/(2*sigl**2) + (np.sin(pa)**2)/(2*sigm**2)
+    b = (np.sin(2*pa))/(4*sigl**2) - (np.sin(2*pa))/(4*sigm**2)
+    c = (np.sin(pa)**2)/(2*sigl**2) + (np.cos(pa)**2)/(2*sigm**2)
+
+
+    return a,b,c
+
+def gausslm_abc2sig(a,b,c):
+    """
+    Calculate the Gaussian sigma, and position angle from the polynomial 
+    representation.
+
+    Parameters:
+    ----------
+    a : float
+        x axis coefficient.
+    b : float
+        correlation coefficient.
+    c : float
+        y axis coefficient.
+
+    Returns:
+    ----------
+    sigl : float
+        Gaussian major axis.
+    sigm : float
+        Gaussian minor axis.
+    pa : float
+        Gaussian position angle in radians. 
+    """
+
+    pa=np.pi/2 + 0.5*np.arctan2(2*b,(a-c))
+    
+    sigl = np.sqrt(0.5/(a*np.cos(pa)**2 + 2*b*np.cos(pa)*np.sin(pa) + \
+                    c*np.sin(pa)**2))
+    
+    sigm = np.sqrt(0.5/(a*np.sin(pa)**2 - 2*b*np.cos(pa)*np.sin(pa) + \
+                    c*np.cos(pa)**2))
+
+    return sigl,sigm,pa
+
+def gaussuv_sig2abc(sigl,sigm,pa):
+    """
+    Calculate the 2D Gaussian parameters in the polynomial ax**2 + 2bxy +cy**2.
+
+    Parameters:
+    ----------
+    sigl : float
+        Gaussian major axis.
+    sigm : float
+        Gaussian minor axis.
+    pa : float
+        Gaussian position angle in radians. 
+
+    Returns:
+    ----------
+    alpha : float
+        x axis coefficient.
+    beta : float
+        correlation coefficient.
+    gamma : float
+        y axis coefficient.
+    """
+
+    alpha = 2*(np.pi*sigl*np.cos(pa))**2 + 2*(np.pi*sigm*np.sin(pa))**2
+    beta = (np.sin(2*pa)) * ((np.pi*sigl)**2 - (np.pi*sigm)**2)
+    gamma = 2*(np.pi*sigl*np.sin(pa))**2 + 2*(np.pi*sigm*np.cos(pa))**2
+
+
+    return alpha,beta,gamma
+
+def gaussuv_abc2sig(alpha,beta,gamma):
+    """
+    Calculate the Gaussian sigma, and position angle from the polynomial 
+    representation. This calculated the sigma and pa from the uv space (a,b,c),
+    here we call them alpha, beta, gamma to distinguish we are in Fourier space.
+
+    Parameters:
+    ----------
+    alpha : float
+        x axis coefficient.
+    beta : float
+        correlation coefficient.
+    gamma : float
+        y axis coefficient.
+
+    Returns:
+    ----------
+    sigl : float
+        Gaussian major axis.
+    sigm : float
+        Gaussian minor axis.
+    pa : float
+        Gaussian position angle in radians. 
+    """
+
+    pa = 0.5*np.arctan2(2*beta,(alpha - gamma))
+
+    sigl = np.sqrt(alpha*np.cos(pa)**2 + 2*beta*np.cos(pa)*np.sin(pa) + \
+                    gamma*np.sin(pa)**2)/(np.sqrt(2)*np.pi)
+    
+    sigm = np.sqrt(alpha*np.sin(pa)**2 - 2*beta*np.cos(pa)*np.sin(pa) + \
+                    gamma*np.cos(pa)**2)/(np.sqrt(2)*np.pi)
+
+    return sigl,sigm,pa
