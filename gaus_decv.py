16ac4cd3955afb061bcc94b88e57253c44be1e08
JaidenCook <jaiden.cook@student.curtin.edu.au>
2024-03-22 15:30:31 +0800

    Copied old deconv2 function

--- gaus_decv.py
+++ gaus_decv.py
@@ -1,18 +1,16 @@
 import numpy as np
 
 def deconv2(gaus_bm, gaus_c):
-    """ 
-    Deconvolves gaus_bm from gaus_c to give gaus_dc.
-    Stolen shamelessly from Miriad gaupar.for.
-    All PA is in degrees.
+    """ Deconvolves gaus_bm from gaus_c to give gaus_dc.
+        Stolen shamelessly from Miriad gaupar.for.
+        All PA is in degrees.
 
-    Returns deconvolved gaussian parameters and flag:
+        Returns deconvolved gaussian parameters and flag:
      0   All OK.
      1   Result is pretty close to a point source.
      2   Illegal result.
 
-    """
-    
+        """
     from math import pi, cos, sin, atan2, sqrt
 
     rad = 180.0/pi
@@ -28,15 +26,13 @@ def deconv2(gaus_bm, gaus_c):
 
     alpha = ( (bmaj1*cos(theta1))**2 + (bmin1*sin(theta1))**2 -
               (bmaj2*cos(theta2))**2 - (bmin2*sin(theta2))**2 )
-
     beta = ( (bmaj1*sin(theta1))**2 + (bmin1*cos(theta1))**2 -
              (bmaj2*sin(theta2))**2 - (bmin2*cos(theta2))**2 )
-
     gamma = 2.0 * ( (bmin1**2-bmaj1**2)*sin(theta1)*cos(theta1) -
                   (bmin2**2-bmaj2**2)*sin(theta2)*cos(theta2) )
 
-    s = alpha + beta # 
-    t = sqrt((alpha-beta)**2 + gamma**2) # Sqrt of the cosine law
+    s = alpha + beta
+    t = sqrt((alpha-beta)**2 + gamma**2)
     limit = min(bmaj1, bmin1, bmaj2, bmin2)
     limit = 0.1*limit*limit
 
@@ -61,6 +57,7 @@ def deconv2(gaus_bm, gaus_c):
             bpa = rad * 0.5 * atan2(-gamma, alpha-beta)
         ifail = 0
     return (bmaj, bmin, bpa), ifail
+
 
 def gausslm_sig2abc(sigl,sigm,pa):
     """
