0caeee55f66bcb71d16d83fb479952cc4e0da599
JaidenCook <jaiden.cook@student.curtin.edu.au>
2024-04-18 15:46:37 +0800

    Refactored the spectral Bayesian fitting.

--- src_specfit.py
+++ src_specfit.py
@@ -89,82 +89,89 @@ def spec_fit(freqs,fluxVec,func=power_law,sigma=None,bounds=True,
 
     return popt,pcov
 
-def logposterior(theta, data, sigma, x):
+def uniformPrior(val,hypParams):
     """
-    The natural logarithm of the joint posterior.
+    Returns the natural log of the uniform prior without normalisation.
 
     Parameters:
     ----------
-    theta : tuple
+    val : float
         A sample containing individual parameter values.
-    data : numpy ndarray
-        The set of data/observations.
-    sigma : float, or numpy ndarray
-        The standard deviation of the data points.
-    x : numpy ndarray
-        The abscissa values at which the data/model is defined.
-    
+    hypParams : tuple, or list, or np.ndarray
+        A sample containing individual parameter values.
+
     Returns:
     ----------
+    logPrior : float
+        Natural log of the uniform prior.
     """
 
-    lp = logprior(theta) # get the prior
+    low = hypParams[0]
+    high = hypParams[1]
 
-    # if the prior is not finite return a probability of zero (log probability of -inf)
-    if not np.isfinite(lp):
-        return -np.inf
+    logPrior = 0 if low < val < high else -np.inf
 
-    # return the likeihood times the prior (log likelihood plus the log prior)
-    return lp + loglikelihood(theta, data, sigma, x)
+    return logPrior
 
-def loglikelihood(theta, data, sigma, freqs):
+def normalPrior(val,hypParams):
     """
-    The natural logarithm of the joint Gaussian likelihood.
+    Returns the natural log of the normal prior without normalisation.
 
     Parameters:
     ----------
-    theta : tuple
+    val : float
         A sample containing individual parameter values.
-    data : numpy ndarray
-        The set of data/observations.
-    sigma : float, or numpy ndarray
-        The standard deviation of the data points.
-    x : numpy ndarray
-        The abscissa values at which the data/model is defined.
+    hypParams : tuple, or list, or np.ndarray
+        A sample containing individual parameter values.
 
-    Note:
-        We do not include the normalisation constants (as discussed above).
-    
     Returns:
     ----------
+    logPrior : float
+        Natural log of the uniform prior.
     """
 
-    # unpack the model parameters from the tuple
-    S0, alpha = theta
+    mu = hypParams[0]
+    sig = hypParams[1]
 
-    # evaluate the model (assumes that the straight_line model is defined as above)
-    md = power_law(freqs/freqs[0], S0, alpha)
+    logPrior = -0.5*((val-mu)/sig)**2
 
-    if isinstance(sigma, np.ndarray):
-        if len(sigma.shape) > 1 and (sigma.shape[0]==sigma.shape[1]):
-            # If the covariance matrix is given.
-            resid = (md - data)
-            Kinv = np.linalg.inv(sigma)
-            #print(resid.shape,np.matmul(resid,sigma).shape)
-            return -0.5*np.matmul(np.matmul(resid,Kinv),resid.T)
-        else:
-            return -0.5 * np.sum(((md - data) / sigma)**2)
-    else:
-        return -0.5 * np.sum(((md - data) / sigma)**2)
+    return logPrior
 
+def lognormalPrior(val,hypParams):
+    """
+    Returns the natural log of the normal prior without normalisation.
 
-def logprior(theta,uniform=False):
+    Parameters:
+    ----------
+    val : float
+        A sample containing individual parameter values.
+    p0Dict : tuple, or list, or np.ndarray
+        A sample containing individual parameter values.
+
+    Returns:
+    ----------
+    logPrior : float
+        Natural log of the uniform prior.
+    """
+
+    mu = hypParams[0]
+    sig = hypParams[1]
+
+    # Log normal distribution satistics.
+    logMu = np.log(mu**2/np.sqrt(mu**2 + sig**2))
+    logSig = np.log(1 + (sig/mu)**2)
+
+    logPrior = -0.5*((np.log(val)-logMu)/logSig)**2
+
+    return logPrior
+
+def calc_logPriors(thetaVec,paramsDict):
     """
     The natural logarithm of the prior probability.
 
     Parameters:
     ----------
-    theta : tuple
+    thetaVec : tuple
         A sample containing individual parameter values.
     uniform : bool, default=False
 
@@ -173,44 +180,180 @@ def logprior(theta,uniform=False):
     
     Returns:
     ----------
+    logPrior : float
+        Natural log of the priors.
     """
 
-    lp = 0.
+    paramKeys = list(paramsDict.keys())
+
+    logPrior = 0
+    for ind,theta in enumerate(thetaVec):
+        # Get the prior dictionary for the input parameter.
+        p0Dict = paramsDict[paramKeys[ind]]
+
+        if p0Dict['prior'] == 'uniform':
+            logPrior += uniformPrior(theta,p0Dict['hyperparams'])
+        elif p0Dict['prior'] == 'normal':
+            logPrior += normalPrior(theta,p0Dict['hyperparams'])
+        elif p0Dict['prior'] == 'lognormal':
+            logPrior += lognormalPrior(theta,p0Dict['hyperparams'])
+
+    return logPrior
+
+def logposterior(theta,data,sigma,paramsDict,xVec,model=power_law):
+    """
+    The natural logarithm of the joint posterior. Not normalised.
+
+    Parameters:
+    ----------
+    theta : tuple
+        A sample containing individual parameter values.
+    data : numpy ndarray
+        The set of data/observations.
+    sigma : float, or numpy ndarray
+        The standard deviation of the data points.
+    paramsDict : dict
+        Dictionary containing the hyperparameters for the model params.
+    xVec : numpy ndarray
+        The abscissa values at which the data/model is defined.
+    model : function, default=power_law
+        Model that describes the data. Default is the power law.
+    
+    Returns:
+    ----------
+    logPosterior : float
+        Natural log of the posterior distribution.
+    """
+
+    lp = calc_logPriors(theta,paramsDict) # get the prior
+
+    # if the prior is not finite return a probability of zero (log probability of -inf)
+    if not np.isfinite(lp):
+        return -np.inf
+
+    # return the likeihood times the prior (log likelihood plus the log prior)
+    logPosterior = lp + loglikelihood(theta,data,sigma,xVec,model=model)
+
+    return logPosterior
+
+def loglikelihood(theta,data,sigma,freqs,model=power_law):
+    """
+    The natural logarithm of the joint Gaussian likelihood.
+
+    TODO add the curved power law model.
+
+    Note:
+        We do not include the normalisation constants (as discussed above).
+
+    Parameters:
+    ----------
+    theta : tuple
+        A sample containing individual parameter values.
+    data : numpy ndarray
+        The set of data/observations.
+    sigma : float, or numpy ndarray
+        The standard deviation of the data points.
+    freqs : numpy ndarray
+        The abscissa values at which the data/model is defined.
+    model : function, default=power_law
+        Model that describes the data. Default is the power law.
+
+    
+    Returns:
+    ----------
+    logl : float
+        Evaluated log likelihood.
+    """
 
     # unpack the model parameters from the tuple
-    S0, alpha = theta
+    if model==power_law:
+        S0, alpha = theta
+        # Evaluate the model.
+        mod_data = model(freqs/freqs[0], S0, alpha)
 
-    if uniform:
-        S0min = 0. # lower range of prior
-        S0max = 10.  # upper range of prior
-        
-        #
-        lp = 0. if S0min < S0 < S0max else -np.inf
-        
-        alpha_min = -3
-        alpha_max = 0
-        
-        #
-        lp -= 0. if alpha_min < alpha < alpha_max else -np.inf
-    else:
-        S0mu = 0.115
-        S0sig = 0.164
-        logS0mu = np.log(S0mu**2/np.sqrt(S0mu**2 + S0sig**2))
-        logS0sig = np.log(1 + (S0sig/S0mu)**2)
-        lp = lp - (0.5*((np.log(S0)-logS0mu)/logS0sig)**2)
+    # Residual of the model and the data.
+    resid = mod_data - data
 
-        # Gaussian prior on m
-        alpha_mu = -0.75 # mean of the Gaussian prior
-        alpha_sigma = 0.3 # standard deviation of the Gaussian prior
-        
-        #
-        lp = lp - 0.5*((alpha-alpha_mu)/alpha_sigma)**2
-    
+    if isinstance(sigma, np.ndarray):
+        if len(sigma.shape) > 1 and (sigma.shape[0]==sigma.shape[1]):
+            # If the covariance matrix is given.
+            Kinv = np.linalg.inv(sigma)
+            logl = -0.5*np.matmul(np.matmul(resid,Kinv),resid.T)
+        else:
+            # If only a vector of uncertainties is given.
+            logl = -0.5 * np.sum((resid/sigma)**2)
 
-    return lp
+    return logl
 
-def bayes_spec_fit(freqs,flux,sigma=None):
+
+def initial_samples(paramsDict,Nens=100,p0cond=False):
     """
+    Create the initial sample array for the ensemblers. Use the hyper parameters
+    to determine the intil samples. 
+
+    Parameters:
+    ----------
+    paramsDict : dict
+        Dictionary containing the hyperparameters for the model params.
+    Nens : int, default=100
+        Number of points in the ensemble.
+    
+    Returns:
+    ----------
+    paramsSamples : float, numpy ndarray
+        Initial ensemble parameters.
+    """
+    # Get the parameter dictionary keys.
+    paramKeys = list(paramsDict.keys())
+    # Set the parameter sample array.
+    #paramsSamples = np.zeros((len(paramKeys),Nens))
+    paramsSamples = np.zeros((Nens,len(paramKeys)))
+
+    for ind,key in enumerate(paramKeys):
+        
+        # Get the parameter dictionary.
+        p0Dict = paramsDict[key]
+        # Get the hyperparameters.
+        hypParams = p0Dict['hyperparams']
+        
+        # If initial values are given, and p0cond = True, then use the initial
+        # values in place of the hyperparameters. This is useful if you fit
+        # with least squares first to get a good guess.
+        try:
+            if p0cond:
+                hypParams[0] = p0Dict['p0']
+        except KeyError:
+            pass
+
+        try:
+            if p0cond:
+                hypParams[1] = p0Dict['e_p0']
+        except KeyError:
+            pass
+
+        if p0Dict['prior'] == 'uniform':
+            pVec = np.random.uniform(hypParams[0],hypParams[1],Nens)
+        elif p0Dict['prior'] == 'normal':
+            pVec = np.random.normal(hypParams[0],hypParams[1], Nens) 
+        elif p0Dict['prior'] == 'lognormal':
+            mu = hypParams
+            sig = hypParams
+
+            # Log normal distribution satistics.
+            logMu = np.log(mu**2/np.sqrt(mu**2 + sig**2))
+            logSig = np.log(1 + (sig/mu)**2)
+
+            pVec = np.random.lognormal(logMu,logSig,Nens)
+
+        #paramsSamples[ind,:] = pVec
+        paramsSamples[:,ind] = pVec
+
+    return paramsSamples
+
+def bayes_spec_fit(freqs,flux,paramsDict,sigma=None,
+                   Nburnin=500,Nsamples=1250,Nens=100,p0cond=False,corner=False):
+    """
+    Fit the parameters using Bayesian inference.
     
     Parameters:
     ----------
@@ -225,57 +368,62 @@ def bayes_spec_fit(freqs,flux,sigma=None):
     Returns:
     ----------
     """
+    # Get the initial samples.
+    inisamples = initial_samples(paramsDict,Nens=Nens,p0cond=p0cond)
+    # Number of parameters/dimensions
+    ndims = inisamples.shape[1] 
 
-    Nens = 100   # number of ensemble points
-
-    # Gaussian prior on m
-    alpha_mu = -0.75     # mean of the Gaussian prior
-    alpha_sigma = np.sqrt(2) # standard deviation of the Gaussian prior
-    #alpha_sigma = 0.6 # standard deviation of the Gaussian prior
-    #alpha_min = -3
-    #alpha_max = 0
-
-    alpha_ini = np.random.normal(alpha_mu, alpha_sigma, Nens) # initial m points
-    #alpah_ini = np.random.uniform(alpha_min, alpha_max, Nens) # initial m points
-
-    S0min = 0.  # lower range of prior
-    S0max = 10.   # upper range of prior
-    S0mu = 0.1
-    S0sig = 1/np.sqrt(2)
-    #logS0mu = np.log(S0mu**2/np.sqrt(S0mu**2 + S0sig**2))
-    #logS0sig = np.log(1 + (S0sig/S0mu)**2)
-
-    S0ini = np.random.uniform(S0min,S0max,Nens) # initial c points
-    #S0ini = np.random.lognormal(logS0mu,logS0sig,Nens)
-
-    inisamples = np.array([S0ini,alpha_ini]).T # initial samples
-
-    ndims = inisamples.shape[1] # number of parameters/dimensions
-
-    Nburnin = 500   # number of burn-in samples
-    Nsamples = 1250  # number of final posterior samples
-
-    # for bookkeeping set number of likelihood calls to zero
+    # For bookkeeping set number of likelihood calls to zero
     loglikelihood.ncalls = 0
 
-    # set additional args for the posterior (the data, the noise std. dev., and the abscissa)
-    #argslist = (Sdata1, stdVec, freqs)
-    argslist = (flux, sigma, freqs)
+    # Set additional args for the posterior 
+    # (the data, the noise std. dev., and the abscissa).
+    #argslist = (flux,sigma,paramsDict,freqs)
+    argslist = (flux,sigma,paramsDict,freqs,power_law)
 
-    # set up the sampler
-    sampler = emcee.EnsembleSampler(Nens, ndims, logposterior, args=argslist)
+    # Set up the sampler.
+    sampler = emcee.EnsembleSampler(Nens,ndims,logposterior,args=argslist)
 
-    # pass the initial samples and total number of samples required
+    # Pass the initial samples and total number of samples required
     sampler.run_mcmc(inisamples, Nsamples + Nburnin)
 
-    # extract the samples (removing the burn-in)
+    # Extract the samples (removing the burn-in)
     samples_emcee = sampler.get_chain(flat=True, discard=Nburnin)
 
-    # lets store some results for showing together later
-    popt = np.array([np.round(np.mean(samples_emcee[:,0]),5),
-                     np.round(np.mean(samples_emcee[:,1]),5)])
-    perr = np.array([np.round(np.std(samples_emcee[:,0]),5),
-                     np.round(np.std(samples_emcee[:,1]),5)])
+    # Store the results.
+    popt = np.nanmean(samples_emcee,axis=0)
+    perr = np.nanstd(samples_emcee,axis=0)
 
-    #
+    if corner:
+        # If True plot the samples.
+        plotposts(samples_emcee,paramsDict=paramsDict,popt=popt)
+
     return popt,perr
+
+# plot the resulting posteriors
+def plotposts(samples,paramsDict,popt=None, **kwargs):
+    """
+    Function to plot posteriors using corner.py and scipy's gaussian KDE function.
+    """
+    # functions for plotting posteriors
+    import corner
+    from scipy.stats import gaussian_kde
+    
+    paramsKeys = list(paramsDict.keys())
+    labels = [paramsDict[key]['label'] for key in paramsKeys]
+    if "truths" not in kwargs:
+        if np.any(popt):
+            kwargs["truths"] = [popt[0], popt[1]]
+        else:
+            kwargs["truths"] = [paramsDict[paramsKeys[0]]['bounds'][0], 
+                                paramsDict[paramsKeys[1]]['bounds'][0]]
+
+    fig = corner.corner(samples, labels=labels, 
+                        hist_kwargs={'density': True}, **kwargs)
+
+    # plot KDE smoothed version of distributions
+    for axidx, samps in zip([0, 3], samples.T):
+        kde = gaussian_kde(samps)
+        xvals = fig.axes[axidx].get_xlim()
+        xvals = np.linspace(xvals[0], xvals[1], 100)
+        fig.axes[axidx].plot(xvals, kde(xvals), color='firebrick')
